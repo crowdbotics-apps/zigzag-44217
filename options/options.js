7845da6b069bab46011e51c0dcc6086cf2b1b6e9
Crowdbotics <team@crowdbotics.com>
2023-10-30 14:59:57 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "zigzag-44217",
+  url: "https://zigzag-44217.botics.co",
+  api: "https://zigzag-44217.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
